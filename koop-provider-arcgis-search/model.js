c54ae3a4cdebf3d998eef96f7b2b492633ebafdf
ajturner <aturner@esri.com>
2017-08-22 23:01:47 -0400

    Added explicit fields

--- koop-provider-arcgis-search/model.js
+++ koop-provider-arcgis-search/model.js
@@ -8,6 +8,43 @@
 const request = require('request-promise').defaults({gzip: true, json: true})
 const config = require('config')
 const _maxPageSize = 100;
+const _fieldDictionary = [ { "name": "id", "type": "esriFieldTypeString", "alias": "id", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "owner", "type": "esriFieldTypeString", "alias": "owner", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "created", "type": "esriFieldTypeDate", "alias": "created", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "modified", "type": "esriFieldTypeDate", "alias": "modified", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "guid", "type": "esriFieldTypeString", "alias": "guid", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "name", "type": "esriFieldTypeString", "alias": "name", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "title", "type": "esriFieldTypeString", "alias": "title", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "type", "type": "esriFieldTypeString", "alias": "type", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "typeKeywords", "type": "esriFieldTypeString", "alias": "typeKeywords", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "description", "type": "esriFieldTypeString", "alias": "description", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "tags", "type": "esriFieldTypeString", "alias": "tags", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "snippet", "type": "esriFieldTypeString", "alias": "snippet", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "thumbnail", "type": "esriFieldTypeString", "alias": "thumbnail", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "documentation", "type": "esriFieldTypeString", "alias": "documentation", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "extent", "type": "esriFieldTypeString", "alias": "extent", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "categories", "type": "esriFieldTypeString", "alias": "categories", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "spatialReference", "type": "esriFieldTypeString", "alias": "spatialReference", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "accessInformation", "type": "esriFieldTypeString", "alias": "accessInformation", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "licenseInfo", "type": "esriFieldTypeString", "alias": "licenseInfo", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "culture", "type": "esriFieldTypeString", "alias": "culture", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "properties", "type": "esriFieldTypeString", "alias": "properties", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "url", "type": "esriFieldTypeString", "alias": "url", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "proxyFilter", "type": "esriFieldTypeString", "alias": "proxyFilter", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "access", "type": "esriFieldTypeString", "alias": "access", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "size", "type": "esriFieldTypeInteger", "alias": "size", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "appCategories", "type": "esriFieldTypeString", "alias": "appCategories", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "industries", "type": "esriFieldTypeString", "alias": "industries", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "languages", "type": "esriFieldTypeString", "alias": "languages", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "largeThumbnail", "type": "esriFieldTypeString", "alias": "largeThumbnail", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "banner", "type": "esriFieldTypeString", "alias": "banner", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "screenshots", "type": "esriFieldTypeString", "alias": "screenshots", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "listed", "type": "esriFieldTypeString", "alias": "listed", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "numComments", "type": "esriFieldTypeInteger", "alias": "numComments", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "numRatings", "type": "esriFieldTypeInteger", "alias": "numRatings", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "avgRating", "type": "esriFieldTypeInteger", "alias": "avgRating", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "numViews", "type": "esriFieldTypeInteger", "alias": "numViews", "length": null, "editable": false, "nullable": true, "domain": null },
+{ "name": "OBJECTID", "type": "esriFieldTypeOID", "alias": "ID", "length": null, "editable": false, "nullable": false, "domain": null } ]
 
 function Model (koop) {}
 
@@ -31,7 +68,7 @@ Model.prototype.getData = function (req, callback) {
   let query = {f: 'json'}
   query.q = req.query.where || '*'
 
-  query.num = req.query.resultRecordCount || 10
+  query.num = req.query.resultRecordCount || 5000
   query.start = req.query.resultOffset || 1
   let orderBy = req.query.orderByFields
   if(orderBy !== undefined && orderBy !== null) {
@@ -46,7 +83,8 @@ Model.prototype.getData = function (req, callback) {
   requests.push(request(url))
 
   // Multiple pages
-  while(req.query.resultRecordCount > _maxPageSize && query.start < req.query.resultRecordCount) {
+  while(req.query.resultRecordCount > _maxPageSize
+        && query.start < req.query.resultRecordCount) {
     query.start = parseInt(query.start) + _maxPageSize
     console.log("Query", `${portal}?${serializeQueryParams(query)}`)
     requests.push(request(`${portal}?${serializeQueryParams(query)}`))
@@ -58,12 +96,14 @@ Model.prototype.getData = function (req, callback) {
         // translate the response into geojson
 
         console.log("Pages|", pages.length)
-        const items = pages.reduce((collection, page) => {
+        const items = {}
+        items.total = pages[0].total
+
+        items.results = pages.reduce((collection, page) => {
+          console.log("Count|", page.total)
           console.log("Putting page", page.results.length)
           return collection.concat(page.results)
         }, [])
-
-        console.log("Items|", items.length)
 
         const geojson = translate(items)
         // Cache data for 10 seconds at a time by setting the ttl or "Time to Live"
@@ -75,52 +115,7 @@ Model.prototype.getData = function (req, callback) {
           name: "Search", // Get the workbook name before ! symbol and set as layer name
           description: 'Collaborate in Google docs, analyse in ArcGIS',
           displayField: 'title',
-          "fields": [
-{
-"name": "created",
-"type": "esriFieldTypeDate",
-"alias": "created",
-"length": null,
-"editable": false,
-"nullable": true,
-"domain": null
-},
-{
-"name": "title",
-"type": "esriFieldTypeString",
-"alias": "title",
-"length": null,
-"editable": false,
-"nullable": true,
-"domain": null
-},
-{
-"name": "extent",
-"type": "esriFieldTypeString",
-"alias": "extent",
-"length": null,
-"editable": false,
-"nullable": true,
-"domain": null
-},
-{
-"name": "OBJECTID",
-"type": "esriFieldTypeOID",
-"alias": "ID",
-"length": null,
-"editable": false,
-"nullable": false,
-"domain": null
-},{
-"name": "typeKeywords",
-"type": "esriFieldTypeString",
-"alias": "typeKeywords",
-"length": null,
-"editable": false,
-"nullable": true,
-"domain": null
-}
-]
+          fields: _fieldDictionary
         }
         // hand off the data to Koop
         callback(null, geojson)
@@ -133,28 +128,27 @@ function translate (input) {
   // console.log("translate", input)
   const features = {
     type: 'FeatureCollection',
-    features: input.map(formatFeature),
+    features: input.results.map(formatFeature),
     count: input.total
   }
   return features;
 }
 
-function formatFeature (vehicle) {
+function formatFeature (input) {
   // Most of what we need to do here is extract the longitude and latitude
   let ring = []
-  if(vehicle.extent.length == 0) {
-    vehicle.extent = [[0,0], [1,1]]
+  if(input.extent.length == 0) {
+    input.extent = [[0,0], [1,1]]
   }
-  console.log("Vehicle", vehicle)
-  ring.push([vehicle.extent[0][0], vehicle.extent[0][1]])
-  ring.push([vehicle.extent[0][0], vehicle.extent[1][1]])
-  ring.push([vehicle.extent[1][0], vehicle.extent[1][1]])
-  ring.push([vehicle.extent[1][0], vehicle.extent[0][1]])
-  ring.push([vehicle.extent[0][0], vehicle.extent[0][1]])
-  console.log("Vehicle rings", ring)
+  ring.push([input.extent[0][0], input.extent[0][1]])
+  ring.push([input.extent[0][0], input.extent[1][1]])
+  ring.push([input.extent[1][0], input.extent[1][1]])
+  ring.push([input.extent[1][0], input.extent[0][1]])
+  ring.push([input.extent[0][0], input.extent[0][1]])
+
   const feature = {
     type: 'Feature',
-    properties: vehicle,
+    properties: input,
     geometry: {
       "type": "Polygon",
       "coordinates": [ring]
